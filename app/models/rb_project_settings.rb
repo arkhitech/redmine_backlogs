4cfe41b8c61da34f2044f4516ee6b5dc923d47df
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2015-07-08 10:50:53 +0000

    fix project settings backlog tab

--- app/models/rb_project_settings.rb
+++ app/models/rb_project_settings.rb
@@ -2,5 +2,6 @@ class RbProjectSettings < ActiveRecord::Base
   unloadable
   attr_protected :created_at # hack, all attributes will be mass asigment
   belongs_to :project
+  attr_accessible :project_id
 end
 
